d7643a807fb5a4a81be820b5d9ad1734edddc1b5
Anthony White <anthony.white.j@gmail.com>
2011-08-10 22:07:12 -0400

    Testing pre-commit hook

---
+++ app.js
@@ -0,0 +1,16 @@
+(function() {
+  var app, express;
+  express = require('express');
+  app = express.createServer();
+  app.register('.coffee', require('coffeekup'));
+  app.set('view engine', 'coffee');
+  app.use(express.static(__dirname + '/public'));
+  app.get('/', function(request, response) {
+    return response.render('index', {
+      title: "Tasks"
+    });
+  });
+  app.listen(process.env.PORT || 3000, function() {
+    return console.log("Listening on port " + (process.env.PORT || 3000));
+  });
+}).call(this);
